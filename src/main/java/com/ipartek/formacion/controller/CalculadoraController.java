e78a31e04ea3b4fe2164c5ddf52cc3cfc4f459f6
asalbidegoitia <asisalbide@gmail.com>
2019-07-24 11:50:26 +0200

    Calculadora hecha

---
+++ src/main/java/com/ipartek/formacion/controller/CalculadoraController.java
@@ -0,0 +1,170 @@
+package com.ipartek.formacion.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.formacion.controller.pojo.Alert;
+
+/**
+ * Servlet implementation class CalculadoraController
+ */
+@WebServlet("/calculadora")
+public class CalculadoraController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static final String VIEW_INDEX = "calculadora/index.jsp";
+	public static String view = VIEW_INDEX;
+    
+	public static final String OP_SUMAR = "sumar";
+	public static final String OP_RESTAR = "restar";
+	public static final String OP_MULTIPLICAR = "multiplicar";
+	public static final String OP_DIVIDIR = "dividir";
+	
+	private static double numero1= 0.0;
+	private static double numero2= 0.0;
+	private Double resultado = 0.0;
+	private static String op;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalculadoraController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		op = request.getParameter("op");
+		if (op == null) {
+			//TODO
+		}
+
+		switch (op) {
+		case OP_SUMAR:
+			sumar(request, response);
+			break;
+		case OP_RESTAR: 
+			restar(request, response);
+			break;
+		case OP_MULTIPLICAR:
+			multiplicar(request, response);
+			break;
+		case OP_DIVIDIR: 
+			dividir(request, response);
+			break;
+		default:
+			//TODO
+			break;
+		}
+		request.setAttribute("op", op);
+		request.getRequestDispatcher(view).forward(request, response);
+
+	}
+
+	private void sumar(HttpServletRequest request, HttpServletResponse response) {
+		String datoNum1= request.getParameter("numero1");
+		if(datoNum1.isEmpty()) {
+			numero1= resultado;
+		}else {
+			numero1 = Double.parseDouble(datoNum1);	
+		}
+		
+		String datoNum2= request.getParameter("numero2");
+		if(!datoNum2.isEmpty()) {
+			numero2 = Double.parseDouble(datoNum2);
+		}	
+		
+		resultado= numero1+numero2;
+		request.setAttribute("numero1", numero1 );
+		request.setAttribute("numero2", numero2 );
+		request.setAttribute("resultado", resultado );
+	
+	}
+
+	private void restar(HttpServletRequest request, HttpServletResponse response) {
+		String datoNum1= request.getParameter("numero1");
+		if(datoNum1.isEmpty()) {
+			numero1= resultado;
+		}else {
+			numero1 = Double.parseDouble(datoNum1);	
+		}
+		
+		String datoNum2= request.getParameter("numero2");
+		if(!datoNum2.isEmpty()) {
+			numero2 = Double.parseDouble(datoNum2);
+		}	
+		
+		resultado= numero1-numero2;
+		request.setAttribute("numero1", numero1 );
+		request.setAttribute("numero2", numero2 );
+		request.setAttribute("resultado", resultado );
+		
+	}
+	
+	private void multiplicar(HttpServletRequest request, HttpServletResponse response) {
+		String datoNum1= request.getParameter("numero1");
+		if(datoNum1.isEmpty()) {
+			numero1= resultado;
+		}else {
+			numero1 = Double.parseDouble(datoNum1);	
+		}
+		
+		String datoNum2= request.getParameter("numero2");
+		if(!datoNum2.isEmpty()) {
+			numero2 = Double.parseDouble(datoNum2);
+		}	
+		
+		resultado= numero1*numero2;
+		request.setAttribute("numero1", numero1 );
+		request.setAttribute("numero2", numero2 );
+		request.setAttribute("resultado", resultado );
+		
+	}
+	
+	private void dividir(HttpServletRequest request, HttpServletResponse response) {
+		String datoNum1= request.getParameter("numero1");
+		if(datoNum1.isEmpty()) {
+			numero1= resultado;
+		}else {
+			numero1 = Double.parseDouble(datoNum1);	
+		}
+		
+		String datoNum2= request.getParameter("numero2");
+		if (datoNum2.equals("0.0")) {
+			request.setAttribute("mensaje", new Alert("danger","Error no se puede dividir entre 0"));
+			
+		}else {	
+			if(!datoNum2.isEmpty()) {
+				numero2 = Double.parseDouble(datoNum2);
+			} 
+			resultado= numero1/numero2;
+		}
+		
+		request.setAttribute("numero1", numero1 );
+		request.setAttribute("numero2", numero2 );
+		request.setAttribute("resultado", resultado );
+		
+	}
+
+}
---
+++ src/main/webapp/calculadora/index.jsp
@@ -0,0 +1,41 @@
+<%@page import="com.ipartek.formacion.controller.CalculadoraController"%>
+<%@include file="../includes/header.jsp"%>
+<%@include file="../includes/navbar.jsp"%>
+
+<%@include file="../includes/mensaje.jsp"%>
+
+<h1>Calculadora</h1>
+<form action="calculadora" method="post">
+	<div class="form-group">
+		<label for="inputNum1"
+			class="col-sm-3 col-form-label col-form-label-lg">Número 1</label>
+			 <input
+			type="number" step="0.01" class="form-control" name="numero1" id="inputNum1"
+			placeholder="Introduce un número" value="${numero1}">
+	</div>
+	
+	<div class="form-group">
+		<label for="inputNum2"
+			class="col-sm-3 col-form-label col-form-label-lg">Número 2</label> 
+			<input
+			type="number" step="0.01" class="form-control" name="numero2" id="inputNum2"
+			placeholder="Introduce un número" value="${numero2}">
+	</div>
+	<select name="op" class="form-control" value="${op}">
+		<option value="">Selecciona una operación</option>
+ 		<option value="<%= CalculadoraController.OP_SUMAR %>">Sumar</option>
+ 		<option value="<%= CalculadoraController.OP_RESTAR %>">Restar</option>
+ 		<option value="<%= CalculadoraController.OP_MULTIPLICAR %>">Multiplicar</option>
+ 		<option value="<%= CalculadoraController.OP_DIVIDIR %>">Dividir</option>
+	</select>
+
+
+	<button type="submit" class="btn btn-primary mt-2 mb-2 btn-block">Calcular</button>
+</form>	
+	<div class="resultado">
+		<label for="resultado"
+			class="col-sm-3 col-form-label col-form-label-lg">Resultado: ${resultado}
+		</label>
+	</div>
+
+<%@include file="../includes/footer.jsp"%>
