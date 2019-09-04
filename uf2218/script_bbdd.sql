a89a2d2f1bc6d9622c7a8252d5bc4829f2be3d43
anderuraga <ander.uraga@gmail.com>
2019-09-04 13:24:23 +0200

    tabla usuario

--- uf2218/script_bbdd.sql
+++ uf2218/script_bbdd.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `v2019` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `v2019`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: v2019
+-- Host: localhost    Database: v2019
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.1.38-MariaDB
 
@@ -18,6 +18,32 @@ USE `v2019`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `contrasenya` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'admin','admin'),(2,'pepe','pepe'),(3,'lucas','lucas'),(7,'patxi','12345678');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `video`
 --
 
@@ -28,8 +54,11 @@ CREATE TABLE `video` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(150) NOT NULL,
   `codigo` varchar(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `codigo_UNIQUE` (`codigo`)
+  UNIQUE KEY `codigo_UNIQUE` (`codigo`),
+  KEY `FK_VIDEO_USUARIO_idx` (`id_usuario`),
+  CONSTRAINT `FK_VIDEO_USUARIO` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,9 +68,17 @@ CREATE TABLE `video` (
 
 LOCK TABLES `video` WRITE;
 /*!40000 ALTER TABLE `video` DISABLE KEYS */;
-INSERT INTO `video` VALUES (1,'Su Ta Gar - Mari  Su Ta Gar 20 Urte','5oi2WTPAHcs'),(5,'Clasicos del Rock','BWcmD_de99g'),(9,'The Black Eyed Peas - Where Is The Love? (Official Music Video)','WpYeekQkAdc'),(10,'Bob Marley - redemption song','kOFu6b3w6c0'),(16,'El Where','i_cVJgIz_Cs'),(17,'Red Hot Chili Peppers - Dark Necessities','Q0oIoR9mLwc');
+INSERT INTO `video` VALUES (1,'Su Ta Gar - Mari  Su Ta Gar 20 Urte','5oi2WTPAHcs',1),(5,'Clasicos del Rock','BWcmD_de99g',1),(9,'The Black Eyed Peas - Where Is The Love? (Official Music Video)','WpYeekQkAdc',1),(10,'Bob Marley - redemption song','kOFu6b3w6c0',1),(16,'El Where','i_cVJgIz_Cs',1),(17,'Red Hot Chili Peppers - Dark Necessities','Q0oIoR9mLwc',1);
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'v2019'
+--
+
+--
+-- Dumping routines for database 'v2019'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +89,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-29 13:36:19
+-- Dump completed on 2019-09-04 13:23:40
