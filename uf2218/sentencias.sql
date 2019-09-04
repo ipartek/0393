5a112c5b1fe374f6d01ec6c797bde0d7490b0e04
anderuraga <ander.uraga@gmail.com>
2019-09-04 08:48:27 +0200

    TODO SQLs tabla usuario

--- uf2218/sentencias.sql
+++ uf2218/sentencias.sql
@@ -1,7 +1,14 @@
 USE v2019;
 
-
+-- listar todos
 -- SELECT id,nombre,contrasenya FROM usuario ORDER BY id DESC LIMIT 500;
 
 -- busqueda por nombre
 SELECT id,nombre,contrasenya FROM usuario WHERE nombre LIKE '%cas%' ORDER BY nombre ASC LIMIT 500;
+
+
+-- eliminar un usuario por id
+
+-- crear un usuario nuevo
+
+-- modificar un usuario
