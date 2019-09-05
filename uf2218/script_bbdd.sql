0370dbc3a6c38d0c66c7520cb3d6090330a60770
anderuraga <ander.uraga@gmail.com>
2019-09-05 09:22:09 +0200

    nuevas tablas de categoria y likes

--- uf2218/script_bbdd.sql
+++ uf2218/script_bbdd.sql
@@ -1,92 +1,91 @@
-CREATE DATABASE  IF NOT EXISTS `v2019` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `v2019`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
---
--- Host: localhost    Database: v2019
--- ------------------------------------------------------
--- Server version	5.5.5-10.1.38-MariaDB
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
---
--- Table structure for table `usuario`
---
+-- -----------------------------------------------------
+-- Schema videos
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `contrasenya` varchar(45) NOT NULL,
+-- -----------------------------------------------------
+-- Schema videos
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `v2019` DEFAULT CHARACTER SET utf8 ;
+USE `v2019` ;
+
+-- -----------------------------------------------------
+-- Table `videos`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `videos`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `contrasenya` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC))
+ENGINE = InnoDB;
 
---
--- Dumping data for table `usuario`
---
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'admin','admin'),(2,'pepe','pepe'),(3,'lucas','lucas'),(7,'patxi','12345678');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `video`
---
-
-DROP TABLE IF EXISTS `video`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `video` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(150) NOT NULL,
-  `codigo` varchar(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `videos`.`categoria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `videos`.`categoria` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `codigo_UNIQUE` (`codigo`),
-  KEY `FK_VIDEO_USUARIO_idx` (`id_usuario`),
-  CONSTRAINT `FK_VIDEO_USUARIO` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC))
+ENGINE = InnoDB;
 
---
--- Dumping data for table `video`
---
 
-LOCK TABLES `video` WRITE;
-/*!40000 ALTER TABLE `video` DISABLE KEYS */;
-INSERT INTO `video` VALUES (1,'Su Ta Gar - Mari  Su Ta Gar 20 Urte','5oi2WTPAHcs',1),(5,'Clasicos del Rock','BWcmD_de99g',1),(9,'The Black Eyed Peas - Where Is The Love? (Official Music Video)','WpYeekQkAdc',1),(10,'Bob Marley - redemption song','kOFu6b3w6c0',1),(16,'El Where','i_cVJgIz_Cs',1),(17,'Red Hot Chili Peppers - Dark Necessities','Q0oIoR9mLwc',1);
-/*!40000 ALTER TABLE `video` ENABLE KEYS */;
-UNLOCK TABLES;
+-- -----------------------------------------------------
+-- Table `videos`.`video`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `videos`.`video` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(150) NOT NULL,
+  `codigo` VARCHAR(11) NOT NULL,
+  `usuario_id` INT NOT NULL,
+  `categoria_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `codigo_UNIQUE` (`codigo` ASC),
+  INDEX `fk_video_usuario_idx` (`usuario_id` ASC),
+  INDEX `fk_video_categoria1_idx` (`categoria_id` ASC),
+  CONSTRAINT `fk_video_usuario`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `videos`.`usuario` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_video_categoria`
+    FOREIGN KEY (`categoria_id`)
+    REFERENCES `videos`.`categoria` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT)
+ENGINE = InnoDB;
 
---
--- Dumping events for database 'v2019'
---
 
---
--- Dumping routines for database 'v2019'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- -----------------------------------------------------
+-- Table `videos`.`likes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `videos`.`likes` (
+  `usuario_id` INT NOT NULL,
+  `video_id` INT NOT NULL,
+  `fecha` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`usuario_id`, `video_id`),
+  INDEX `fk_usuario_has_video_video1_idx` (`video_id` ASC),
+  INDEX `fk_usuario_has_video_usuario1_idx` (`usuario_id` ASC),
+  CONSTRAINT `fk_usuario_likes_video`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `videos`.`usuario` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_video_likes_usuario`
+    FOREIGN KEY (`video_id`)
+    REFERENCES `videos`.`video` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-04 13:23:40
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
