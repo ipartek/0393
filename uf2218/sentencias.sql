6ed5fa8e5747d29c53f263cb0e4aa5091e864c1b
anderuraga <ander.uraga@gmail.com>
2019-09-05 10:36:49 +0200

    joins

--- uf2218/sentencias.sql
+++ uf2218/sentencias.sql
@@ -15,3 +15,37 @@ INSERT INTO usuario ( nombre, contrasenya) VALUES ( ? , ?);
 
 -- modificar un usuario
 UPDATE usuario SET nombre= ?, contrasenya= ? WHERE id = ?;
+
+
+-- numero de likes del Fary
+-- SELECT COUNT(*) as 'numero_likes' FROM likes WHERE video_id = 4;
+
+-- INNER JOIN EXPLICITA
+-- mostrar los videos del usuario 'soso' por su id
+SELECT 
+	u.nombre as 'usuario',
+    v.nombre as 'video',
+    c.nombre as 'categoria'
+FROM 
+	usuario as u INNER JOIN video as v ON u.id = v.usuario_id 
+    INNER JOIN categoria as c ON c.id = v.categoria_id 
+    
+WHERE
+	u.id = 3;
+
+-- INNER JOIN IMPLICITA	
+-- mostrar los videos del usuario 'soso' por su id
+SELECT 
+	u.nombre as 'usuario',
+    v.nombre as 'video',
+    c.nombre as 'categoria'
+FROM 
+	usuario as u,
+    video as v,
+    categoria as c
+   
+WHERE
+	u.id = v.usuario_id AND
+    v.categoria_id = c.id;	
+	
+	
